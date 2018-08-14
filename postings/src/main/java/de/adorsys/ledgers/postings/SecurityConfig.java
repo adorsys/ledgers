2ed81ed73630aac1bc9cce54a35f35a7dafdc89b
Vladimir Nechai <vne@adorsys.de>
2018-08-14 15:33:12 +0200

    add SecurityConfig to application

---
+++ postings/src/main/java/de/adorsys/ledgers/postings/SecurityConfig.java
@@ -0,0 +1,21 @@
+package de.adorsys.ledgers.postings;
+
+import java.security.Principal;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public Principal getPrincipal(){
+        return new Principal() {
+
+            @Override
+            public String getName() {
+                return "anonymous";
+            }
+        };
+    }
+}
