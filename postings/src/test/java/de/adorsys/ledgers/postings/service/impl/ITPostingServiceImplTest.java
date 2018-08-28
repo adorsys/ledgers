cd00df0479573a7cbc9ea02be32cfb68e6c60161
Vladimir Nechai <vne@adorsys.de>
2018-08-28 16:16:51 +0200

    add newPosting test method

--- postings/src/test/java/de/adorsys/ledgers/postings/service/impl/ITPostingServiceImplTest.java
+++ postings/src/test/java/de/adorsys/ledgers/postings/service/impl/ITPostingServiceImplTest.java
@@ -3,9 +3,15 @@ package de.adorsys.ledgers.postings.service.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assume.assumeNotNull;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import de.adorsys.ledgers.postings.domain.*;
+import de.adorsys.ledgers.postings.repository.LedgerRepository;
+import de.adorsys.ledgers.postings.repository.PostingRepository;
+import de.adorsys.ledgers.postings.utils.Ids;
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +26,6 @@ import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
 
-import de.adorsys.ledgers.postings.domain.ChartOfAccount;
-import de.adorsys.ledgers.postings.domain.LedgerAccountType;
 import de.adorsys.ledgers.postings.repository.LedgerAccountTypeRepository;
 import de.adorsys.ledgers.postings.service.ChartOfAccountService;
 import de.adorsys.ledgers.tests.PostingsApplication;
@@ -34,8 +38,25 @@ import de.adorsys.ledgers.tests.PostingsApplication;
 @DatabaseTearDown(value={"ITPostingServiceImplTest-db-entries.xml"}, type=DatabaseOperation.DELETE_ALL)
 public class ITPostingServiceImplTest {
 
+    @Autowired
+    LedgerRepository ledgerRepository;
+
+    @Autowired
+    PostingServiceImpl postingService;
+
     @Test
     public void test_newPosting_ok() {
+        Optional<Ledger> ledgerOption = ledgerRepository.findById("Zd0ND5YwSzGwIfZilhumPg");
+        Assume.assumeTrue(ledgerOption.isPresent());
+        Posting posting = Posting.builder()
+                .id(Ids.id())
+                .recordUser("recUser")
+                .oprId("oprId")
+                .oprDetails("oprDetails")
+                .pstTime(LocalDateTime.now())
+                .pstType(PostingType.BAL_STMT)
+                .ledger(ledgerOption.get())
+                .build();
 
     }
 }
