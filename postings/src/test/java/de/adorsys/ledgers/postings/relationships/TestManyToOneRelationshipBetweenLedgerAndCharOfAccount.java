0efd27a9913f54e67c2690ceff84d9be90a5769e
Vladimir Nechai <vne@adorsys.de>
2018-08-29 17:38:37 +0200

    implement test_2_ledgers_same_coa

--- postings/src/test/java/de/adorsys/ledgers/postings/relationships/TestManyToOneRelationshipBetweenLedgerAndCharOfAccount.java
+++ postings/src/test/java/de/adorsys/ledgers/postings/relationships/TestManyToOneRelationshipBetweenLedgerAndCharOfAccount.java
@@ -5,9 +5,15 @@ import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import de.adorsys.ledgers.postings.domain.ChartOfAccount;
+import de.adorsys.ledgers.postings.domain.Ledger;
+import de.adorsys.ledgers.postings.repository.ChartOfAccountRepository;
+import de.adorsys.ledgers.postings.repository.LedgerRepository;
 import de.adorsys.ledgers.tests.PostingsApplication;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -23,8 +29,25 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 @DatabaseTearDown(value={"TestManyToOneRelationshipBetweenLedgerAndCharOfAccount-db-entries.xml"}, type=DatabaseOperation.DELETE_ALL)
 public class TestManyToOneRelationshipBetweenLedgerAndCharOfAccount {
 
-    @Test
-    public void test_leger_has_many_ledger_accounts() {
+    @Autowired
+    ChartOfAccountRepository chartOfAccountRepository;
 
+    @Autowired
+    LedgerRepository ledgerRepository;
+
+
+    @Test
+    public void test_2_ledgers_same_coa() {
+        ChartOfAccount coa = chartOfAccountRepository.findOptionalByName("IFRS").orElse(null);
+        Assert.assertNotNull(coa);
+
+        Ledger ledger1 = ledgerRepository.findById("Zd0ND5YwSzGwIfZilhumPg").orElse(null);
+        Assert.assertNotNull(ledger1);
+
+        Ledger ledger2 = ledgerRepository.findById("Zd0ND5YwSzGwIfZilhumPg1").orElse(null);
+        Assert.assertNotNull(ledger2);
+
+        // CoA of 2 ledgers is the same
+        Assert.assertEquals(ledger1.getCoa().getId(), ledger2.getCoa().getId());
     }
 }
