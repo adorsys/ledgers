394242e5b24fb1f1bca656e3e54ba60114462a84
sautiev <isa@adorsys.de>
2018-09-11 13:40:59 +0200

    created test class created test db

---
+++ postings/src/test/java/de/adorsys/ledgers/postings/relationships/TestManyToOneRelationshipBetweenLedgerAccountTypeAndChartOfAccount.java
@@ -0,0 +1,42 @@
+package de.adorsys.ledgers.postings.relationships;
+
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import de.adorsys.ledgers.postings.domain.ChartOfAccount;
+import de.adorsys.ledgers.postings.domain.Ledger;
+import de.adorsys.ledgers.postings.repository.ChartOfAccountRepository;
+import de.adorsys.ledgers.postings.repository.LedgerRepository;
+import de.adorsys.ledgers.tests.PostingsApplication;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes=PostingsApplication.class)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+@DatabaseSetup("TestManyToOneRelationshipBetweenLedgerAccountTypeAndChartOfAccount-db-entries.xml")
+@DatabaseTearDown(value={"TestManyToOneRelationshipBetweenLedgerAccountTypeAndChartOfAccount-db-entries.xml"}, type=DatabaseOperation.DELETE_ALL)
+
+
+public class TestManyToOneRelationshipBetweenLedgerAccountTypeAndChartOfAccount {
+
+    @Autowired
+    ChartOfAccountRepository chartOfAccountRepository;
+
+    @Autowired
+    LedgerRepository ledgerRepository;
+
+
+
+}
