5d38e789f6fd238772d2727d90a20742d105798b
fpo <fpo@adorsys.com>
2018-09-26 19:02:24 -0400

    model simplification

--- postings/src/main/java/de/adorsys/ledgers/postings/domain/Ledger.java
+++ postings/src/main/java/de/adorsys/ledgers/postings/domain/Ledger.java
@@ -2,6 +2,7 @@ package de.adorsys.ledgers.postings.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -23,6 +24,13 @@ public class Ledger extends NamedEntity {
 	@ManyToOne(optional=false)
 	@Getter
 	private ChartOfAccount coa;
+	
+	/*
+	 * This field is used to secure the timestamp of the ledger opening.
+	 * A posting time can not be carry a posting 
+	 */
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime lastClosing;
 
 	@Builder
 	public Ledger(String id, LocalDateTime created, String user, String shortDesc, String longDesc, String name,
--- postings/src/main/java/de/adorsys/ledgers/postings/domain/PostingLine.java
+++ postings/src/main/java/de/adorsys/ledgers/postings/domain/PostingLine.java
@@ -1,16 +1,19 @@
 package de.adorsys.ledgers.postings.domain;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Embeddable
@@ -43,4 +46,65 @@ public class PostingLine {
 	 */
 	private String srcAccount;
 
+	//================================================================================
+	// Denormalization layer. All following fields:
+	//   - are duplicated from the post
+	//   - never change
+	// They allow
+	//   - decentralization of record data for close accounting period
+	//   - Simple computation of balances
+	//
+	
+	/* The time of recording of this posting. */
+	@Column(nullable = false, updatable = false)
+	@Setter
+	private LocalDateTime recordTime;
+	
+	/*
+	 * The unique identifier of this business operation. 
+	 * If tow posting have the same operation id, the one with the youngest
+	 * record time
+	 */
+	@Column(nullable = false, updatable = false)
+	@Setter
+	private String oprId;
+
+	/*
+	 * This is the time from which the posting is effective in this account
+	 * statement.
+	 */
+	@Column(nullable = false, updatable = false)
+	@Setter
+	private LocalDateTime pstTime;
+	
+	/*
+	 * Some posting are mechanical and do not have an influence on the balance
+	 * of an account. Depending on the business logic of the product module,
+	 * different types of posting might be defined so that the journal can be
+	 * used to document all events associated with an account.
+	 * 
+	 * For a mechanical posting, the same account and amounts must appear in the
+	 * debit and the credit side of the posting. Some account statement will not
+	 * display mechanical postings while producing the user statement.
+	 */
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, updatable = false)
+	@Setter
+	private PostingType pstType;
+	
+	/*
+	 * The ledger governing this posting.
+	 */
+	@ManyToOne(optional=false)
+	@Setter
+	private Ledger ledger;
+
+	/*
+	 * This field is used to secure the timestamp of the ledger opening.
+	 * A posting time can not be carry a posting 
+	 */
+	@Column(nullable = false, updatable = false)
+	@Setter
+	private LocalDateTime lastClosing;
+	
 }
