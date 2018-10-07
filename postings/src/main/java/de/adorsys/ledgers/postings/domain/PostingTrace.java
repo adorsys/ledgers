10cda2130e0ac06e5b61afde835b85f4483ba91a
fpo <fpo@adorsys.com>
2018-10-07 10:21:41 +0200

    Fixing posting traces tests #14

--- postings/src/main/java/de/adorsys/ledgers/postings/domain/PostingTrace.java
+++ postings/src/main/java/de/adorsys/ledgers/postings/domain/PostingTrace.java
@@ -1,8 +1,16 @@
 package de.adorsys.ledgers.postings.domain;
 
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import de.adorsys.ledgers.postings.utils.RecordHashHelper;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +26,7 @@ import lombok.ToString;
  * Since posting never change, any statement produced by this module can be
  * reproduced or checked for integrity.
  * 
- * Each trace entry keeps reference of the antecedent posting trace.
+ * Each trace entry keeps reference of an antecedent posting trace.
  * 
  * The hash value of this posting also includes:
  * - The hash value of the input posting
@@ -41,15 +49,19 @@ public class PostingTrace {
 	/*
 	 * The position of the target posting in the list.
 	 */
+	@Column(nullable = false, updatable = false)
 	private int pos;
 	
 	/*The source posting id*/
+	@Column(nullable = false, updatable = false)
 	private String srcPstId;
 	
 	/*The hash value of the src posting*/
+	@Column(nullable = false, updatable = false)
 	private String srcPstHash;
 
 	/*The target posting id. Posting receiving.*/
+	@Column(nullable = false, updatable = false)
 	private String tgtPstId;
 	
 	/*Id of the antecedent trace.*/
@@ -57,13 +69,21 @@ public class PostingTrace {
 
 	/*Hash value of the antecedent trace.*/
 	private String antTraceHash;
+
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime recordTime;
 	
 	/*
 	 *The hash value of this trace 
 	 */
 	@Setter
+	@Column(nullable = false, updatable = false)
 	private String hash;
 
+	@Setter
+	@Column(nullable = false, updatable = false)
+	private String hashAlg;
+	
 	@Builder
 	public PostingTrace(String id, int pos, String srcPstId, String srcPstHash, String tgtPstId, String antTraceId,
 			String antTraceHash) {
@@ -76,4 +96,19 @@ public class PostingTrace {
 		this.antTraceId = antTraceId;
 		this.antTraceHash = antTraceHash;
 	}
+	
+	private static final RecordHashHelper RECORD_HASH_HELPER = new RecordHashHelper();
+
+	@PrePersist
+	public void hash() {
+		if (hash != null)
+			throw new IllegalStateException("Can not update a posting trace.");
+		recordTime = LocalDateTime.now();
+		try {
+			hash = RECORD_HASH_HELPER.computeRecHash(this);
+		} catch (NoSuchAlgorithmException | JsonProcessingException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+	
 }
--- postings/src/main/java/de/adorsys/ledgers/postings/utils/RecordHashHelper.java
+++ postings/src/main/java/de/adorsys/ledgers/postings/utils/RecordHashHelper.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.adorsys.ledgers.postings.domain.Posting;
+import de.adorsys.ledgers.postings.domain.PostingTrace;
 
 public class RecordHashHelper {
 
@@ -29,6 +30,24 @@ public class RecordHashHelper {
 		// Get string value including hash
 		MessageDigest digest = MessageDigest.getInstance(posting.getRecordHashAlg());
 		byte[] valueAsBytes = OBJECT_MAPPER.writeValueAsBytes(posting);
+		byte[] hash = digest.digest(valueAsBytes);
+		return Hex.encodeHexString(hash);
+	}
+
+	public String computeRecHash(PostingTrace postingTrace) throws NoSuchAlgorithmException, JsonProcessingException {
+		if(postingTrace.getAntTraceId()!=null && postingTrace.getAntTraceHash()==null)
+			throw new IllegalStateException(String.format("Missing record antecedant hash. Must be set to hash of %s prior saving.", postingTrace.getAntTraceId()));
+
+		// Reset Record hash if not null.
+		if(postingTrace.getHash()!=null)
+			throw new IllegalStateException("PostingTrace contains a hash. Set hash to null before calling this method");
+		
+		// Check hash algo
+		if(postingTrace.getHashAlg()==null)postingTrace.setHashAlg(DEFAULT_HASH_ALG);
+		
+		// Get string value including hash
+		MessageDigest digest = MessageDigest.getInstance(postingTrace.getHashAlg());
+		byte[] valueAsBytes = OBJECT_MAPPER.writeValueAsBytes(postingTrace);
 		byte[] hash = digest.digest(valueAsBytes);
 		return Hex.encodeHexString(hash);
 	}
