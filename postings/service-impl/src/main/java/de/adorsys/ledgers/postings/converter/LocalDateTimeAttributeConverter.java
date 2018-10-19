1a79518b5690282c7f6774b980d903817fd050c1
Dmitry Mishchuk <dmi@adorsys.com.ua>
2018-10-19 12:42:07 +0300

    fix pmd

--- postings/service-impl/src/main/java/de/adorsys/ledgers/postings/converter/LocalDateTimeAttributeConverter.java
+++ postings/service-impl/src/main/java/de/adorsys/ledgers/postings/converter/LocalDateTimeAttributeConverter.java
@@ -10,12 +10,16 @@ public class LocalDateTimeAttributeConverter implements AttributeConverter<Local
 
     @Override
     public Timestamp convertToDatabaseColumn(LocalDateTime locDateTime) {
-        return locDateTime == null ? null : Timestamp.valueOf(locDateTime);
+        return locDateTime == null
+                       ? null
+                       : Timestamp.valueOf(locDateTime);
     }
 
     @Override
     public LocalDateTime convertToEntityAttribute(Timestamp sqlTimestamp) {
-        return sqlTimestamp == null ? null : sqlTimestamp.toLocalDateTime();
+        return sqlTimestamp == null
+                       ? null
+                       : sqlTimestamp.toLocalDateTime();
     }
 
 }
