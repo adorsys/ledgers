775f6586576dfdf16c49efaab0a6e2757ddac60b
Vladimir Nechai <vne@adorsys.de>
2018-10-24 12:50:46 +0200

    fix pmd

--- user-management/repository/src/main/java/de/adorsys/ledgers/um/db/domain/ScaUserData.java
+++ user-management/repository/src/main/java/de/adorsys/ledgers/um/db/domain/ScaUserData.java
@@ -1,7 +1,5 @@
 package de.adorsys.ledgers.um.db.domain;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 import org.jetbrains.annotations.NotNull;
 
 import javax.persistence.*;
