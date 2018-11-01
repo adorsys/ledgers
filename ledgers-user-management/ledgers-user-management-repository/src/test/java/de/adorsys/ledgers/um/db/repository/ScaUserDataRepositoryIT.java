6499cfd37752c9014cd6936304ef82293ff3aa9d
fpo <fpo@adorsys.de>
2018-11-01 11:12:32 -0400

    activated all integration tests. removed unnecessary tests

--- ledgers-user-management/ledgers-user-management-repository/src/test/java/de/adorsys/ledgers/um/db/repository/ScaUserDataRepositoryIT.java
+++ ledgers-user-management/ledgers-user-management-repository/src/test/java/de/adorsys/ledgers/um/db/repository/ScaUserDataRepositoryIT.java
@@ -1,25 +1,32 @@
 package de.adorsys.ledgers.um.db.repository;
 
-import com.github.springtestdbunit.annotation.DatabaseOperation;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.DatabaseTearDown;
-import de.adorsys.ledgers.um.db.domain.ScaMethodType;
-import de.adorsys.ledgers.um.db.domain.ScaUserDataEntity;
-import de.adorsys.ledgers.um.db.domain.UserEntity;
-import de.adorsys.ledgers.util.Ids;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.util.Assert;
 
-@Ignore
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+
+import de.adorsys.ledgers.um.db.domain.ScaMethodType;
+import de.adorsys.ledgers.um.db.domain.ScaUserDataEntity;
+import de.adorsys.ledgers.um.db.domain.UserEntity;
+import de.adorsys.ledgers.util.Ids;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = UmRepositoryApplication.class)
-@DatabaseSetup("SCAMethodsRepositoryTest-db-entries.xml")
-@DatabaseTearDown(value={"SCAMethodsRepositoryTest-db-entries.xml"}, type= DatabaseOperation.DELETE_ALL)
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+    TransactionalTestExecutionListener.class,
+    DbUnitTestExecutionListener.class})
+@DatabaseSetup("ScaUserDataRepositoryIT-db-entries.xml")
+@DatabaseTearDown(value={"db-delete-all.xml"}, type= DatabaseOperation.DELETE_ALL)
 public class ScaUserDataRepositoryIT {
 
     @Autowired
