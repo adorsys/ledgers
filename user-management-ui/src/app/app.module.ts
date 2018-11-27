693558fc4b7d5d39e2d1a1aba3203664c2d1e757
Vladimir Nechai <vne@adorsys.de>
2018-11-27 16:12:03 +0100

    navbar and bootstrap theme added

--- user-management-ui/src/app/app.module.ts
+++ user-management-ui/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './commons/navbar/navbar.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
