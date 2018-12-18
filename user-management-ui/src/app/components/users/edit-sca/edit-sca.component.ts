0c2bb477a9f3f4e5aaf974dfa7dfa5e93b1affe8
Vladimir Nechai <vne@adorsys.de>
2018-12-18 15:40:07 +0100

    form controls for edit sca

--- user-management-ui/src/app/components/users/edit-sca/edit-sca.component.ts
+++ user-management-ui/src/app/components/users/edit-sca/edit-sca.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../../../models/user.model";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../../../services/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'edit-sca',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditScaComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  user: User;
+
+  scaForm: FormGroup;
+
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private router: Router,) { }
 
   ngOnInit() {
+    this.scaForm = this.formBuilder.group({
+      scaUserData: this.formBuilder.array([
+        this.initScaData(),
+      ])
+    });
+  }
+
+  get formControl() {
+    return this.scaForm.controls;
+  }
+
+  initScaData() {
+    return this.formBuilder.group({
+      scaMethod: ['', Validators.required],
+      methodValue: ['', Validators.required]
+    })
+  }
+
+  addScaDataItem() {
+    const control = <FormArray>this.scaForm.controls['scaUserData'];
+    control.push(this.initScaData());
+  }
+
+  removeScaDataItem(i: number) {
+    const control = <FormArray>this.scaForm.controls['scaUserData'];
+    control.removeAt(i);
+  }
+
+
+  onSubmit() {
+    console.log(this.scaForm.value);
+    // this.userService.createUser(this.userForm.value)
+    //   .subscribe(response => {
+    //     this.router.navigateByUrl('/users');
+    //   });
   }
 
 }
--- user-management-ui/src/app/components/users/user-new/user-new.component.ts
+++ user-management-ui/src/app/components/users/user-new/user-new.component.ts
@@ -25,7 +25,6 @@ export class UserNewComponent implements OnInit {
 
   ngOnInit() {
     this.setupUserFormControl();
-
   }
 
   setupUserFormControl(): void {
