d7240d325a84865e698e3450312f13e5a3475e0d
Dmytro Mishchuk <dmi@adorsys.com.ua>
2019-03-21 15:49:42 +0000

    fix cors

--- ledgers-app/src/main/java/de/adorsys/ledgers/app/server/WebSecurityConfig.java
+++ ledgers-app/src/main/java/de/adorsys/ledgers/app/server/WebSecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.security.Principal;
-import java.util.Arrays;
 import java.util.Optional;
 
 @Configuration
@@ -30,35 +29,38 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final TokenAuthenticationService tokenAuthenticationService;
 
+    private final CorsConfigProperties corsConfigProperties;
+
     @Autowired
-    public WebSecurityConfig(TokenAuthenticationService tokenAuthenticationService) {
+    public WebSecurityConfig(TokenAuthenticationService tokenAuthenticationService, CorsConfigProperties corsConfigProperties) {
         this.tokenAuthenticationService = tokenAuthenticationService;
+        this.corsConfigProperties = corsConfigProperties;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests().antMatchers(
-            		"/",
-            		"/management/app/admin",
-            		"/management/app/ping", 
-            		"/users/login",
-                    "/users/register",
-            		"/users/loginForConsent",
-            		"/data-test/upload-mockbank-data",
-            		"/data-test/db-flush",
-            		"/staff-access/users/register",
-            		"/staff-access/users/login").permitAll()
-            .and()
-            .authorizeRequests().antMatchers("/index.css", "/img/*", "/favicon.ico").permitAll()
-            .and()
-            .authorizeRequests().antMatchers("/v2/api-docs", "/swagger-resources", "/swagger-ui.html", "/webjars/**").permitAll()
-            .and()
-            .authorizeRequests().antMatchers("/console/**").permitAll()
-			.and()
-			.cors()
-            .and()
-        	.authorizeRequests().anyRequest().authenticated();
+                .authorizeRequests().antMatchers(
+                "/",
+                "/management/app/admin",
+                "/management/app/ping",
+                "/users/login",
+                "/users/register",
+                "/users/loginForConsent",
+                "/data-test/upload-mockbank-data",
+                "/data-test/db-flush",
+                "/staff-access/users/register",
+                "/staff-access/users/login").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/index.css", "/img/*", "/favicon.ico").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/v2/api-docs", "/swagger-resources", "/swagger-ui.html", "/webjars/**").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/console/**").permitAll()
+                .and()
+                .cors()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
         http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
         http.addFilterBefore(new JWTAuthenticationFilter(tokenAuthenticationService), BasicAuthenticationFilter.class);
@@ -75,26 +77,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AccessTokenTO getAccessTokenTO() {
         return auth().map(this::extractToken).orElse(null);
     }
-	
-	/**
-	 * Return Authentication or empty
-	 * 
-	 * @return
-	 */
-	private static Optional<MiddlewareAuthentication> auth(){
-		return SecurityContextHolder.getContext()==null
-				? Optional.empty()
-				: Optional.ofNullable((MiddlewareAuthentication)SecurityContextHolder.getContext().getAuthentication());
-	}
-	
-	private AccessTokenTO extractToken(MiddlewareAuthentication authentication) {
-		return authentication.getBearerToken().getAccessTokenObject();
-	}
+
+    /**
+     * Return Authentication or empty
+     *
+     * @return
+     */
+    private static Optional<MiddlewareAuthentication> auth() {
+        return SecurityContextHolder.getContext() == null
+                       ? Optional.empty()
+                       : Optional.ofNullable((MiddlewareAuthentication) SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    private AccessTokenTO extractToken(MiddlewareAuthentication authentication) {
+        return authentication.getBearerToken().getAccessTokenObject();
+    }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedOrigins(corsConfigProperties.getAllowedOrigins());
+        configuration.setAllowedMethods(corsConfigProperties.getAllowedMethods());
+        configuration.setAllowedHeaders(corsConfigProperties.getAllowedHeaders());
+        configuration.setAllowCredentials(corsConfigProperties.getAllowCredentials());
+        configuration.setMaxAge(corsConfigProperties.getMaxAge());
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
