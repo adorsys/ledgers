2890c78c9fde8025af7874374893ca241d944aa2
Dmytro Mishchuk <dmi@adorsys.com.ua>
2019-07-04 09:44:50 +0000

    removed unused classes updated liquibase scripts to remove obsolete table

--- ledgers-deposit-account/ledgers-deposit-account-repository/src/main/java/de/adorsys/ledgers/deposit/db/repository/ScheduledPaymentOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package de.adorsys.ledgers.deposit.db.repository;
-
-import de.adorsys.ledgers.deposit.db.domain.ScheduledPaymentOrder;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-//TODO REMOVE unused REPO
-public interface ScheduledPaymentOrderRepository extends PagingAndSortingRepository<ScheduledPaymentOrder, String> {
-}
--- ledgers-postings/ledgers-postings-repository/src/main/java/de/adorsys/ledgers/postings/db/domain/PostingSide.java
+++
@@ -1,6 +0,0 @@
-package de.adorsys.ledgers.postings.db.domain;
-
-//TODO unused ENUM to be removed https://git.adorsys.de/adorsys/xs2a/psd2-dynamic-sandbox/issues/195
-public enum PostingSide {
-	D,C;
-}
