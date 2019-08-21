0e80ae0fbf8303156c24facf7a3c5d12cc3c67e5
Petro Rudenko <pru@adorsys.com.ua>
2019-08-21 14:52:04 +0000

    fix swagger

--- ledgers-middleware/ledgers-middleware-rest-server/src/main/java/de/adorsys/ledgers/middleware/rest/resource/TransactionsStaffResource.java
+++ ledgers-middleware/ledgers-middleware-rest-server/src/main/java/de/adorsys/ledgers/middleware/rest/resource/TransactionsStaffResource.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.Map;
 @RestController
 @MiddlewareUserResource
 @RequiredArgsConstructor
+@RequestMapping(TransactionsStaffResourceAPI.BASE_PATH)
 public class TransactionsStaffResource implements TransactionsStaffResourceAPI {
     private final TransactionService transactionService;
 
