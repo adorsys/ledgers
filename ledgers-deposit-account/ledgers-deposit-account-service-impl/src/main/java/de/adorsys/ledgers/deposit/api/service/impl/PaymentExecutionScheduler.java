8e043498fb917fd6c87fa8a23ff24f905e964149
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2020-05-07 12:06:21 +0000

    added accounts filtering in payment scheduler.

--- ledgers-deposit-account/ledgers-deposit-account-service-impl/src/main/java/de/adorsys/ledgers/deposit/api/service/impl/PaymentExecutionScheduler.java
+++ ledgers-deposit-account/ledgers-deposit-account-service-impl/src/main/java/de/adorsys/ledgers/deposit/api/service/impl/PaymentExecutionScheduler.java
@@ -1,6 +1,10 @@
 package de.adorsys.ledgers.deposit.api.service.impl;
 
+import de.adorsys.ledgers.deposit.db.domain.AccountReference;
+import de.adorsys.ledgers.deposit.db.domain.DepositAccount;
 import de.adorsys.ledgers.deposit.db.domain.Payment;
+import de.adorsys.ledgers.deposit.db.domain.PaymentTarget;
+import de.adorsys.ledgers.deposit.db.repository.DepositAccountRepository;
 import de.adorsys.ledgers.deposit.db.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +20,41 @@ import java.util.List;
 public class PaymentExecutionScheduler {
     private static final String SCHEDULER = "Scheduler";
     private final PaymentRepository paymentRepository;
+    private final DepositAccountRepository accountRepository;
+
     private final PaymentExecutionService executionService;
 
     @Scheduled(initialDelayString = "${paymentScheduler.initialDelay}", fixedDelayString = "${paymentScheduler.delay}")
     public void scheduler() {
         log.info("Payment Execution Scheduler started at {}", LocalDateTime.now());
         List<Payment> payments = paymentRepository.getAllDuePayments();
-        payments.forEach(p -> executionService.executePayment(p, SCHEDULER));
+        payments.forEach(this::executeIfNotBlocked);
+    }
+
+    private void executeIfNotBlocked(Payment payment) {
+        boolean debtorIsEnabled = isDebtorAccountEnabled(payment.getAccountId());
+        boolean creditorsAreEnabled = areTargetsEnabled(payment.getTargets());
+
+        if (debtorIsEnabled && creditorsAreEnabled) {
+            executionService.executePayment(payment, SCHEDULER);
+        }
+    }
+
+    private boolean isDebtorAccountEnabled(String accountId) {
+        return accountRepository.findById(accountId)
+                       .map(DepositAccount::isEnabled)
+                       .orElse(false);
+    }
+
+    private boolean areTargetsEnabled(List<PaymentTarget> targets) {
+        return targets.stream()
+                       .map(PaymentTarget::getCreditorAccount)
+                       .allMatch(this::isEnabledCreditorAccount);
+    }
+
+    private boolean isEnabledCreditorAccount(AccountReference reference) {
+        return accountRepository.findByIbanAndCurrency(reference.getIban(), reference.getCurrency())
+                       .map(DepositAccount::isEnabled)
+                       .orElse(true);
     }
 }
---
+++ ledgers-deposit-account/ledgers-deposit-account-service-impl/src/test/java/de/adorsys/ledgers/deposit/api/service/impl/PaymentExecutionSchedulerTest.java
@@ -0,0 +1,118 @@
+package de.adorsys.ledgers.deposit.api.service.impl;
+
+import de.adorsys.ledgers.deposit.db.domain.DepositAccount;
+import de.adorsys.ledgers.deposit.db.domain.Payment;
+import de.adorsys.ledgers.deposit.db.repository.DepositAccountRepository;
+import de.adorsys.ledgers.deposit.db.repository.PaymentRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pro.javatar.commons.reader.YamlReader;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentExecutionSchedulerTest {
+
+    private static final String ACCOUNT_ID = "accountId";
+    private static final String IBAN = "DE91100000000123456789";
+    private static final String SCHEDULER = "Scheduler";
+
+    @InjectMocks
+    private PaymentExecutionScheduler paymentExecutionScheduler;
+
+    @Mock
+    private PaymentRepository paymentRepository;
+    @Mock
+    private DepositAccountRepository accountRepository;
+    @Mock
+    private PaymentExecutionService executionService;
+
+    @Test
+    void scheduler_executed_successfully() {
+        // Given
+        Payment singlePayment = getSinglePayment();
+        when(paymentRepository.getAllDuePayments())
+                .thenReturn(Collections.singletonList(singlePayment));
+        when(accountRepository.findById(ACCOUNT_ID))
+                .thenReturn(java.util.Optional.of(getDepositAccount()));
+        when(accountRepository.findByIbanAndCurrency(IBAN, "EUR"))
+                .thenReturn(java.util.Optional.of(getDepositAccount()));
+
+        // When
+        paymentExecutionScheduler.scheduler();
+
+        // Then
+        verify(executionService, times(1)).executePayment(singlePayment, SCHEDULER);
+    }
+
+    @Test
+    void scheduler_not_executed_debtor_disabled() {
+        // Given
+        Payment singlePayment = getSinglePayment();
+        when(paymentRepository.getAllDuePayments())
+                .thenReturn(Collections.singletonList(singlePayment));
+        DepositAccount blockedAccount = getDepositAccount();
+        blockedAccount.setBlocked(true);
+
+        when(accountRepository.findById(ACCOUNT_ID))
+                .thenReturn(java.util.Optional.of(blockedAccount));
+
+        // When
+        paymentExecutionScheduler.scheduler();
+
+        // Then
+        verify(executionService, never()).executePayment(singlePayment, SCHEDULER);
+    }
+
+    @Test
+    void scheduler_not_executed_creditor_disabled() {
+        // Given
+        Payment singlePayment = getSinglePayment();
+        when(paymentRepository.getAllDuePayments())
+                .thenReturn(Collections.singletonList(singlePayment));
+        when(accountRepository.findById(anyString()))
+                .thenReturn(java.util.Optional.of(getDepositAccount()));
+
+        DepositAccount blockedAccount = getDepositAccount();
+        blockedAccount.setBlocked(true);
+
+        when(accountRepository.findByIbanAndCurrency(IBAN, "EUR"))
+                .thenReturn(Optional.of(blockedAccount));
+        // When
+        paymentExecutionScheduler.scheduler();
+
+        // Then
+        verify(executionService, never()).executePayment(singlePayment, SCHEDULER);
+    }
+
+    private DepositAccount getDepositAccount() {
+        DepositAccount activeAccount = new DepositAccount();
+        activeAccount.setBlocked(false);
+        activeAccount.setSystemBlocked(false);
+
+        return activeAccount;
+    }
+
+    private Payment getSinglePayment() {
+        Payment payment = readFile(Payment.class, "PaymentSingle.yml");
+        payment.getTargets().forEach(t -> t.setPayment(payment));
+        return payment;
+    }
+
+    private <T> T readFile(Class<T> t, String file) {
+        try {
+            return YamlReader.getInstance().getObjectFromResource(DepositAccountPaymentServiceImpl.class, file, t);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("Resource file not found", e);
+        }
+    }
+}
