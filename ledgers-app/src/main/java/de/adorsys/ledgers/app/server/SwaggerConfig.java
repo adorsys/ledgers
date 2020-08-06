9cdc385baf82c4ec12313123fb930bd08917003f
Dmytro Mishchuk <dmi@adorsys.com.ua>
2020-08-07 00:32:11 +0300

    fixed swagger to use OAuth

--- ledgers-app/src/main/java/de/adorsys/ledgers/app/server/SwaggerConfig.java
+++ ledgers-app/src/main/java/de/adorsys/ledgers/app/server/SwaggerConfig.java
@@ -2,6 +2,10 @@ package de.adorsys.ledgers.app.server;
 
 import de.adorsys.ledgers.app.server.auth.KeycloakConfigProperties;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -14,17 +18,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import static io.swagger.v3.oas.annotations.enums.SecuritySchemeType.APIKEY;
-
 
 @Configuration
-//@EnableSwagger2
 @RequiredArgsConstructor
 @SecurityScheme(
-        type = APIKEY,
-        name = "Authorization",
+        type = SecuritySchemeType.OAUTH2,
         scheme = "Bearer",
-        in = SecuritySchemeIn.HEADER)
+        name = "Authorization",
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "Bearer",
+        flows = @OAuthFlows(authorizationCode = @OAuthFlow(
+                authorizationUrl = "${keycloak.auth-server-url}" + "/realms" + "/${keycloak.realm}/" + "protocol/openid-connect/auth",
+                tokenUrl = "${keycloak.auth-server-url}" + "/realms" + "/${keycloak.realm}/" + "protocol/openid-connect/token",
+                scopes = @OAuthScope(name = "global", description = "accessEverything")
+        )))
 public class SwaggerConfig implements WebMvcConfigurer {
     private static final String API_KEY = "apiKey";
     private static final String API_INFO = "api_info.txt";
@@ -51,46 +58,10 @@ public class SwaggerConfig implements WebMvcConfigurer {
     }
 
 //    @Bean
-//    public SecurityConfiguration security() {
-//        return SecurityConfigurationBuilder.builder()
-//                       .clientId(keycloakConfigProp.getResource())
-//                       .clientSecret(keycloakConfigProp.getCredentials().getSecret())
-//                       .realm(keycloakConfigProp.getRealm())
-//                       .appName(keycloakConfigProp.getResource())
-//                       .scopeSeparator(",")
-//                       .useBasicAuthenticationWithAccessCodeGrant(false)
-//                       .build();
-//    }
-//
-//    private OAuth securitySchema() {
-//        GrantType grantType = new AuthorizationCodeGrantBuilder()
-//                                      .tokenEndpoint(new TokenEndpoint(keycloakConfigProp.getRootPath() + "/protocol/openid-connect/token", "Bearer"))
-//                                      .tokenRequestEndpoint(new TokenRequestEndpoint(keycloakConfigProp.getRootPath() + "/protocol/openid-connect/auth", keycloakConfigProp.getResource(), keycloakConfigProp.getCredentials().getSecret()))
-//                                      .build();
-//        return new OAuthBuilder()
-//                       .name(OAUTH2)
-//                       .grantTypes(singletonList(grantType))
-//                       .scopes(scopes())
-//                       .build();
-//    }
-//
-//    private List<AuthorizationScope> scopes() {
-//        return singletonList(new AuthorizationScope("global", "accessEverything"));
-//    }
-
-//    @Bean
 //    public Docket productApi() {
 //        return new Docket(DocumentationType.SWAGGER_2)
-//                       .groupName("001 - LEDGERS API")
-//                       .select()
 //                       .apis(resolvePredicates())
-//                       .paths(PathSelectors.any())
-//                       .build()
-//                       .pathMapping("/")
-//                       .apiInfo(metaData())
-//                       .securitySchemes(Collections.singletonList(apiKey()))
-//                       .securityContexts(Collections.singletonList(securityContext()));
-//
+//                       .build();
 //    }
 //
 //    private Predicate<RequestHandler> resolvePredicates() {
@@ -99,36 +70,5 @@ public class SwaggerConfig implements WebMvcConfigurer {
 //                       ? RequestHandlerSelectors.withClassAnnotation(MiddlewareUserResource.class).or(
 //                                       RequestHandlerSelectors.withClassAnnotation(MiddlewareResetResource.class))
 //                       : RequestHandlerSelectors.withClassAnnotation(MiddlewareUserResource.class);
-//    }
-//
-//    private ApiKey apiKey() {
-//        return new ApiKey(API_KEY, "Authorization", "header");
-//    }
-//
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder()
-//                       .securityReferences(defaultAuth())
-//                       .forPaths(PathSelectors.regex("/*"))
-//                       .build();
-//    }
-//
-//    private List<SecurityReference> defaultAuth() {
-//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//        authorizationScopes[0] = authorizationScope;
-//        return Collections.singletonList(new SecurityReference(API_KEY, authorizationScopes));
-//    }
-//
-//    private ApiInfo metaData() {
-//        Contact contact = new Contact("Adorsys GmbH", "https://www.adorsys.de", "fpo@adorsys.de");
-//
-//        return new ApiInfo(
-//                "Ledgers", fileReader.getStringFromFile(API_INFO),
-//                buildProperties.getVersion() + " " + buildProperties.get("build.number"),
-//                "Terms of Service: to be edited...",
-//                contact,
-//                "Apache License Version 2.0",
-//                "https://www.apache.org/licenses/LICENSE-2.0",
-//                new ArrayList<>());
 //    }
 }
