17e8e777e3435736c037d7a545a2a9d13ff14836
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2020-10-12 16:54:28 +0300

    removed functionality for password resetting (except db table).

--- ledgers-middleware/ledgers-middleware-rest-client/src/main/java/de/adorsys/ledgers/middleware/client/rest/ResetPasswordRestClient.java
+++
@@ -1,8 +0,0 @@
-package de.adorsys.ledgers.middleware.client.rest;
-
-import de.adorsys.ledgers.middleware.rest.resource.ResetPasswordRestAPI;
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(value = "resetPassword", url = LedgersURL.LEDGERS_URL, path = ResetPasswordRestAPI.BASE_PATH)
-public interface ResetPasswordRestClient extends ResetPasswordRestAPI {
-}
--- ledgers-user-management/ledgers-user-management-service-api/src/main/java/de/adorsys/ledgers/um/api/service/ResetPasswordCodeSender.java
+++
@@ -1,8 +0,0 @@
-package de.adorsys.ledgers.um.api.service;
-
-import de.adorsys.ledgers.security.ResetPassword;
-import de.adorsys.ledgers.security.SendCode;
-
-public interface ResetPasswordCodeSender {
-     SendCode sendCode(ResetPassword source);
-}
