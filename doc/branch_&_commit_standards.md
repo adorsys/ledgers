42739db0fff078fa93899dd241b764ec3bb96e6c
ema <enow.mathias-sinke@adorsys.com>
2025-02-19 13:07:22 +0100

    refactor: ema#1923 auto pick up username and issue numb

---
+++ doc/branch_&_commit_standards.md
@@ -0,0 +1,4 @@
+REF: https://adorsys.atlassian.net/wiki/spaces/PSD2/pages/1323040872/Git+Branching+and+Commit+Message+Standards
+
+---
+
--- scripts/branch_commits/initialize-git-standards.sh
+++ scripts/branch_commits/initialize-git-standards.sh
@@ -12,8 +12,4 @@ echo -e "\nSetting up git hooks..."
 chmod 500 ./scripts/branch_commits/setup_hooks.sh
 ./scripts/branch_commits/setup_hooks.sh
 
-# Step 3: Configure git branch creation alias
-echo -e "\nConfiguring git branch creation alias..."
-git config alias.create-branch '!bash ./scripts/branch_commits/create_branch.sh'
-
 echo -e "\nGit standards initialization complete!"
